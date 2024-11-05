29141723498e8e4dce393f65802af28e8bc4cea0
Jade Hagborg <jhagborg@quasicoherent.solutions>
2024-11-05 14:57:41 -0500

    implement ekf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/math/EKF.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/math/EKF.kt
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode.core.math
 
+import org.jetbrains.kotlinx.multik.api.*
+import org.jetbrains.kotlinx.multik.api.linalg.*
+import org.jetbrains.kotlinx.multik.ndarray.data.*
+import org.jetbrains.kotlinx.multik.ndarray.operations.*
+
 sealed class EKFVar() {
-    class StateVar(val name: Int): EKFVar()
-    class NoiseVar(val name: Int): EKFVar()
+    data class StateVar(val name: Int): EKFVar()
+    data class NoiseVar(val name: Int): EKFVar()
 }
 
 typealias EKFExpr = Expr<EKFVar>
 
 interface Noise {
-    fun noise(sd: Double): EKFExpr
+    fun noise(): EKFExpr
 }
 
 interface SetVar {
@@ -58,69 +63,134 @@ interface EKF {
 }
 
 fun ekf(init: InitEKF.() -> Unit) = object : EKF {
+    val dimension: Int
+    var stateMean: D1Array<Double>
+    var stateVar: D2Array<Double>
     init {
+        /*
+         * First, run the user-supplied initializer to figure out how many state
+         * variables there are.
+         */
+        val equations = mutableMapOf<Int, EKFExpr>()
+        var noiseCount = 0
         object : InitEKF {
             override val init = object : SetVar {
                 override fun set(i: Int, expr: EKFExpr) {
-                    TODO("Not yet implemented")
+                    if (i < 0) throw IndexOutOfBoundsException()
+                    equations[i] = expr
                 }
             }
-            override fun noise(sd: Double): EKFExpr {
-                TODO("Not yet implemented")
-            }
+            override fun noise(): EKFExpr =
+                variable(EKFVar.NoiseVar(noiseCount++), 0.0)
         }.init()
+
+        /* Now we can use that to allocate our state. */
+        dimension = equations.size
+        stateMean = mk.zeros(dimension)
+        stateVar = mk.zeros(dimension, dimension)
+
+        /* And update the state with default values. */
+        predict {
+            for (i in 0..<dimension) {
+                new[i] = equations[i] ?: constant(0.0)
+            }
+        }
     }
 
     override fun predict(model: PredictEKF.() -> Unit) {
+        /* Collect information from the user-supplied model. */
+        val equations = mutableMapOf<Int, EKFExpr>()
+        var noiseCount = 0
         object : PredictEKF {
             override val new = object : SetVar {
                 override fun set(i: Int, expr: EKFExpr) {
-                    TODO()
+                    if (i < 0) throw IndexOutOfBoundsException()
+                    if (i >= dimension) throw IndexOutOfBoundsException()
+                    equations[i] = expr
                 }
             }
             override val old = object : GetVar {
                 override fun get(i: Int): EKFExpr {
-                    TODO()
+                    if (i < 0) throw IndexOutOfBoundsException()
+                    if (i >= dimension) throw IndexOutOfBoundsException()
+                    return variable(EKFVar.StateVar(i), stateMean[i])
                 }
             }
-            override fun noise(sd: Double) : EKFExpr {
-                TODO()
-            }
+            override fun noise() : EKFExpr =
+                variable(EKFVar.NoiseVar(noiseCount++), 0.0)
         }.model()
+
+        /* Update the mean estimate for those that changed. */
+        for ((i, expr) in equations) {
+            stateMean[i] = expr.nominal
+        }
+
+        /* Also update the covariance matrix. */
+        val f = mk.d2arrayIndices(dimension, dimension) { i, j ->
+            val eqn = equations[i]
+            if (eqn == null) {
+                if (i == j) 1.0 else 0.0
+            } else {
+                eqn.partials[EKFVar.StateVar(j)] ?: 0.0
+            }
+        }
+        val l = mk.d2arrayIndices(dimension, noiseCount) { i, j ->
+            val eqn = equations[i]
+            if (eqn == null) {
+                if (i == j) 1.0 else 0.0
+            } else {
+                eqn.partials[EKFVar.NoiseVar(j)] ?: 0.0
+            }
+        }
+        stateVar = f dot stateVar dot f.transpose() + l dot l.transpose()
     }
 
     override fun measure(model: MeasureEKF.() -> Unit) {
+        /* Collect the list of measured expressions and values. */
+        val measurements = mutableListOf<Pair<EKFExpr, Double>>()
+        var noiseCount = 0
         object : MeasureEKF {
             override val state = object : GetVar {
-                override fun get(i: Int) : EKFExpr {
-                    TODO()
+                override fun get(i: Int): EKFExpr {
+                    if (i < 0) throw IndexOutOfBoundsException()
+                    if (i >= dimension) throw IndexOutOfBoundsException()
+                    return variable(EKFVar.StateVar(i), stateMean[i])
                 }
             }
             override val measured = object : Measured {
                 override fun set(expr: EKFExpr, value: Double) {
-                    TODO("Not yet implemented")
+                    measurements += Pair(expr, value)
                 }
+            }
+            override fun noise(): EKFExpr =
+                variable(EKFVar.NoiseVar(noiseCount++), 0.0)
+        }.model()
 
-            }
-            override fun noise(sd: Double): EKFExpr {
-                TODO("Not yet implemented")
-            }
+        /* Pack the data into matrices. */
+        val e = mk.d1array(measurements.size) { i ->
+            measurements[i].second - measurements[i].first.nominal
         }
+        val h = mk.d2arrayIndices(measurements.size, dimension) { i, j ->
+            measurements[i].first.partials[EKFVar.StateVar(j)] ?: 0.0
+        }
+        val m = mk.d2arrayIndices(measurements.size, noiseCount) { i, j ->
+            measurements[i].first.partials[EKFVar.NoiseVar(j)] ?: 0.0
+        }
+
+        /* Perform the measurement update. */
+        val v = (h dot stateVar dot h.transpose()) + (m dot m.transpose())
+        val k = stateVar dot h.transpose() dot mk.linalg.inv(v)
+        stateMean = stateMean + (k dot e)
+        stateVar = (mk.identity<Double>(dimension) - (k dot h)) dot stateVar
     }
 
     override val estimate = object : Estimate {
         override val mean = object : Mean {
-            override fun get(i: Int): Double {
-                TODO("Not yet implemented")
-            }
+            override fun get(i: Int) = stateMean[i]
         }
         override val variance = object : Variance {
-            override fun get(i: Int): Double {
-                TODO("Not yet implemented")
-            }
-            override fun get(i: Int, j: Int): Double {
-                TODO("Not yet implemented")
-            }
+            override fun get(i: Int) = stateVar[i, i]
+            override fun get(i: Int, j: Int) = stateVar[i, j]
         }
     }
 }
